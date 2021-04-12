c192a02fdf4dd809d529d653e3001f312613d12d
Willian Alechevez <willian.alechevez@gmail.com>
2021-04-12 02:41:04 -0300

    Commit inicial

---
+++ src/test/java/com/estudoapi/MarsRoverApi/MarsRoverApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudoapi.MarsRoverApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarsRoverApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
